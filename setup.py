0ddcfd87810f754e0fcc287389626a0f5a4e7df3
Damian Pertkiewicz <dpertkie@cisco.com>
2017-10-27 23:22:49 +0100

    Initial commit of lib Created basic list of function, just pushing for test and will optimize it later on

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dreambox',
+  packages = ['dreambox'], 
+  version = '0.1',
+  description = 'Dreambox lib, tested with dm-800HD se',
+  author = 'Damiano Pertolunio',
+  author_email = 'spamtrap@gmail.com',
+  url = 'https://github.com/eneasz/dreambox', 
+  download_url = 'https://github.com/eneasz//dreambox/archive/0.1.tar.gz',
+  keywords = ['Dreambox', 'DM800 HD', 'DM800 HD se', 'Dream', 'Multimedia'], 
+  classifiers = [],
+)
