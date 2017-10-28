ac0961df9d638ae18c76198b3d485e1870cdccf8
Damian Pertkiewicz <dpertkie@cisco.com>
2017-10-28 08:55:26 +0100

    Updating version to force refresh in pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'dreambox',
   packages = ['dreambox'], 
-  version = '0.3',
+  version = '0.4',
   description = 'Dreambox lib, tested with dm-800HD se',
   author = 'Damiano Pertolunio',
   author_email = 'spamtrap@gmail.com',
   url = 'https://github.com/eneasz/dreambox', 
-  download_url = 'https://github.com/eneasz//dreambox/archive/0.1.tar.gz',
+  download_url = 'https://github.com/eneasz//dreambox/archive/0.4.tar.gz',
   keywords = ['Dreambox', 'DM800 HD', 'DM800 HD se', 'Dream', 'Multimedia'], 
   classifiers = [],
 )
